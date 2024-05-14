745d555068da562a49e8d42366c4084765de1e6a
kenfackmerveille868 <kenfackmerveille868@gmail.com>
2024-05-14 13:36:25 +0100

    new repo

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Greeting() {
+    return <h2>My first component</h2>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Greeting/>)
+
+export default Greeting;
