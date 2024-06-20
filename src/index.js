a7bcb431a9c5a701bfbd457ed6f852c5a23c1927
kenfackmerveille868 <kenfackmerveille868@gmail.com>
2024-06-20 17:52:32 +0100

    working

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
-    return <h2>My first component</h2>;
+import "./index.css";
+
+function BookList () {
+    return ( 
+        <section className="bookList">
+            <Book/>
+            <Book/>
+            <Book/>
+            <Book/>
+        </section>
+    );
 }
 
+const Book = () => {
+    return (<article className="book">
+        <Image className="img"/>
+        <Title className="h2"/>
+        <Autor className="h2"/>
+    </article>
+    );
+};
+
+const Image = () => <img 
+src="https://images-na.ssl-images-amazon.com/images/I/917Bc9C1MlL._AC_UL300_SR300,200_.jpg" alt="Iron Flame (The Empyrean, 2) "/>;
+const Title = () => <h2>Iron Flame (The Empyrean, 2) </h2>;
+const Autor = () => {
+    return <h4>Rebecca Yarros </h4>;
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Greeting/>)
-
-export default Greeting;
+root.render(<BookList/>);
--- src/testing.js
+++ src/testing.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const testing = () => {
+const Testing = () => {
   return (
     <div>
       testing
     </div>
   );
 }
+rf
 
-export default testing;
+export default Testing;
